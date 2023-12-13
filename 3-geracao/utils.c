c9478698ecb1570f6e657cb42c65265aff22223d
Mateus Henrique Martins <89797706+mateus0205@users.noreply.github.com>
2023-12-13 15:52:17 -0300

    novas alterações utils.c

--- 3-geracao/utils.c
+++ 3-geracao/utils.c
@@ -1,115 +1,115 @@
 // Tabela de SImbolos
 #include <stdio.h>
 #include <stdlib.h>
-enum
-{
+
+enum {
     INT,
     LOG,
     REG
 };
 
-// TODO: criar lista de campos?
-
-// TODO: criar função para percorrer a lista de campos
-
 char nomeTipo[3][4] = {
-    "INT", "LOG", "REG"};
+    "INT", "LOG", "REG"
+};
+
+// criar uma estrutura e operações para manipular uma lista de campos
+
 #define TAM_TAB 100
 
-typedef struct no *ptno;
-struct no
-{
+// TODO #1 - FEITO
+// Criar lista de campos
+typedef struct listaCampos *ptregistro;
+
+struct listaCampos {
     char nome[100];
     int tipo;
     int pos;
     int desl;
     int tam;
-    ptno prox;
-};
+    ptregistro prox;
+} listaCampos;
 
-ptno insere(ptno L, char nome[100], int tipo, int pos, int desl, int tam)
-{
-    ptno p, novo;
-    novo = (ptno)malloc(sizeof(struct no));
-    strcpy(novo->nome, nome); // na hora de passar novo -> novo = nome deu erro de assigments
-    novo->tipo = tipo;
-    novo->pos = pos;
-    novo->desl = desl;
-    novo->tam = tam;
-    novo->prox = NULL;
+// TODO #2 -
+// Adaptar "insere" do lista.c, feito em aula
+ptregistro insere(ptregistro L, char nome[100], int tipo, int pos, int desl, int tam) {
+    ptregistro p, new;
+    new = (ptregistro)malloc(sizeof(struct listaCampos));
+    strcpy(new->nome, nome);
+    new->tipo = tipo;
+    new->pos = pos;
+    new->desl = desl;
+    new->tam = tam;
+    new->prox = NULL;
     p = L;
-    while (p && p->prox)
+    while (p && p->prox) {
         p = p->prox;
-    if (p)
-        p->prox = novo;
+    }
+    if (p) {
+        p->prox = new;
+    } else {
+        L = new;
+    }
 
-    else
-        L = novo;
     return L;
 }
-ptno busca(ptno L, char *nome)
-{
-    while (L && L->nome != nome)
+
+// TODO #3 -
+// Adaptar "busca" do lista.c, feito em aula
+ptregistro busca(ptregistro L, char *nome) {
+    while (L && L->nome != nome) {
         L = L->prox;
+    }
     return L;
 }
 
-void mostra(ptno L)
-{
-    while (L)
-    {
-        if (L->prox)
-        {
+// TODO #4 -
+// Adaptar "mostra" do lista.c, feito em aula
+void mostra(ptregistro L) {
+    while (L) {
+        if (L->prox) {
             printf("(%s, %s, %d, %d, %d)=>", L->nome, nomeTipo[L->tipo], L->pos, L->desl, L->tam);
-        }
-        else
-        {
+        } else {
             printf("(%s, %s, %d, %d, %d)", L->nome, nomeTipo[L->tipo], L->pos, L->desl, L->tam);
         }
         L = L->prox;
     }
 }
 
-// criar uma estrutura e operações para manipular uma lista de campos
-
-#define TAM_TAB 100
-
-// acrescentar campos na tabela
-struct elemTabSimbolos
-{
-    char id[100]; // nome do identificador
-    int end;      // endereco
-    int tip;      // tipo
-    int tam;
-    int pos;
-    ptno campos;
+// TODO #5 - FEITO
+// Acrescentar campos na tabela
+struct elemTabSimbolos {
+    char id[100];      // nome do identificador
+    int end;           // endereco
+    int tip;           // tipo
+    int tam;           // tamanho
+    int pos;           // posição
+    ptregistro listaCampos; // campos
 } tabSimb[TAM_TAB], elemTab;
 
 int posTab = 0; // indica a próxima posição livre para inserção
 
-int buscaSimbolo(char *s)
-{
+int buscaSimbolo(char *s) {
     int i;
-    for (i = posTab - 1; strcmp(tabSimb[i].id, s) && i >= 0; i--)
+    for (i = posTab - 1; strcmp(tabSimb[i].id, s) && i >= 0; i--){
         ;
-    if (i == -1)
-    {
+    }
+    if (i == -1) {
         char msg[200];
-        sprintf(msg, "Identificador [%s] não ecnontrado!", s);
+        sprintf(msg, "Identificador [%s] não encontrado!", s);
         yyerror(msg);
     }
     return i;
 }
 
-void insereSimbolo(struct elemTabSimbolos elem)
-{
+void insereSimbolo(struct elemTabSimbolos elem) {
     int i;
-    if (posTab == TAM_TAB)
-        yyerror("Tabela de Simbolos cheia!"); //
-    for (i = posTab - 1; strcmp(tabSimb[i].id, elem.id) && i >= 0; i--)
+    if (posTab == TAM_TAB){
+        yyerror("Tabela de Simbolos cheia!");
+    }
+    for (i = posTab - 1; strcmp(tabSimb[i].id, elem.id) && i >= 0; i--){
         ;
-    if (i != -1)
-    {                  // caso encontre um nome igual ao que deseja inserir já existente na tabela
+    }
+    if (i != -1) { // caso encontre um nome igual ao que deseja inserir já existente na tabela
         char msg[200]; // se o valor for igual a -1 a tabela foi totalmente percorrida e não foram encontradas elementos duplicados
         sprintf(msg, "Identificador [%s] duplicado!", elem.id);
         yyerror(msg);
@@ -117,21 +117,23 @@ void insereSimbolo(struct elemTabSimbolos elem)
     tabSimb[posTab++] = elem; // insere o elemento na posição posTab++
 }
 
-void mostraTabela()
-{
-    puts("------------------------------------Tabela de Simbolos------------------------------------");
+// TODO #6 - FEITO
+// Ajustrar "mostraTabela" para mostrar "TAM", "POS" e "CAMPOS"
+void mostraTabela() {
+    puts("----------------------------------- Tabela de Simbolos -----------------------------------");
+    puts("");
     printf("%30s | %3s | %s | %s | %s | %s \n", "ID", "END", "TIP", "TAM", "POS", "CAMPOS");
-    for (int i = 0; i < 90; i++)
+    for (int i = 0; i < 90; i++){
         printf("-");
-    for (int i = 0; i < posTab; i++)
-    {
-        printf("\n%30s | %3d | %s | %d | %d |",
+    }
+    for (int i = 0; i < posTab; i++) {
+        printf("\n%30s | %3d | %s |  %d  |  %d  | ",
                tabSimb[i].id,
                tabSimb[i].end,
                nomeTipo[tabSimb[i].tip],
                tabSimb[i].tam,
                tabSimb[i].pos);
-        mostra(tabSimb[i].campos);
+        mostra(tabSimb[i].listaCampos);
     }
     puts("");
 }
@@ -141,27 +143,27 @@ void mostraTabela()
 int pilha[TAM_PIL];
 int topo = -1;
 
-void empilha(int valor)
-{
-    if (topo == TAM_PIL)
+void empilha(int valor) {
+    if (topo == TAM_PIL){
         yyerror("Pilha semantica cheia!");
+    }
     pilha[++topo] = valor;
 }
 
-int desempilha(void)
-{
-    if (topo == -1)
+int desempilha(void) {
+    if (topo == -1){
         yyerror("Pilha semantica vazia!");
+    }
     return pilha[topo--]; // se a pilha não for vazia retorna a nova posição da pilha após desempilhar
 }
 
 // tipo1 e tipo2 são os tipos esperados na expressão
 // ret é o tipo que será empilhado com resultado da expressão
-void testaTipo(int tipo1, int tipo2, int ret)
-{
+void testaTipo(int tipo1, int tipo2, int ret) {
     int t2 = desempilha();
     int t1 = desempilha();
-    if (t1 != tipo1 || t2 != tipo2)
+    if (t1 != tipo1 || t2 != tipo2){
         yyerror("Incompatibilidade de tipo!");
+    }
     empilha(ret);
 }
